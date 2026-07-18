36f3b1f2cefe6ead73df41e98aafaa117215d792
Bokume2 <nagatomoyuu2@gmail.com>
2026-07-18 22:12:38 +0900

    feat: 初投稿をブーストする機能を追加

--- functions/functions.py
+++ functions/functions.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from mastodon import Mastodon
 from mastodon.return_types import Account, Status
 
-from utils.load_env import MAX_FOLLOWERS
+from utils.load_env import MAX_BOOST_DAYS, MAX_FOLLOWERS
 
 
 def check_public_post(status: Status) -> bool:
@@ -25,3 +27,20 @@ def check_first_post(client: Mastodon, status: Status) -> bool:
 
 def check_beginner(account: Account) -> bool:
     return account.followers_count <= MAX_FOLLOWERS
+
+
+def first_post_boost(client: Mastodon, status: Status) -> None:
+    if check_first_post(client, status) and check_beginner(status.account):
+        client.status_reblog(status)
+
+
+def first_post_boost_scheduled(client: Mastodon) -> None:
+    boost_started = datetime.now()
+    since = boost_started - MAX_BOOST_DAYS
+    while True:
+        tl = client.timeline_local(limit=40, min_id=since)
+        for status in tl:
+            first_post_boost(client, status)
+        if tl[0].created_at >= boost_started or len(tl) < 40:
+            break
+        since = tl[0].created_at
